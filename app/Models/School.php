bb79b6899fc97e4dcb547546ed7e278f94fd8616
DaniKools <44021875+DaniKools@users.noreply.github.com>
2021-06-02 17:32:02 +0200

    1
    1

---
+++ app/Models/School.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class School extends Model
+{
+    use HasFactory;
+
+    public function attendee() {
+        return $this->hasMany(Attendee::class);
+    }
+
+    public function competition() {
+        return $this->belongsTo(Competition::class);
+    }
+}
